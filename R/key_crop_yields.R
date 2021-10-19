50b29661192f6ec16164ee51e86509a033189be3
Siyi Li <slii0048@student.monash.edu>
2021-10-19 19:10:13 +1100

    readme file

---
+++ R/key_crop_yields.R
@@ -0,0 +1,23 @@
+
+#' Titlenof the data
+#'
+#' More details about the data
+#'
+#' @section Background:
+#' The data was collected from ... over the course of ...
+#'
+#' @format A data frme with 53940 rows and 10 variables:
+#'   \describe{
+#'    \item{price}{price, in US dollar}
+#'    \item{carat}{weight of diamonds, in carat}
+#'    ...
+#'  }
+#'
+#' @source where you get the data
+#'
+#'
+
+#'
+#' @examples
+#' print(key_crop_yields)
+"key_crop_yields"
